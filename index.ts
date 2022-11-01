faa892f98ae11887d40ddd4ed774e3ae780efe85
Marcelo Xavier Marquez Alba <xaviermarquez.alba@gmail.com>
2022-11-01 18:33:42 -0300

    FIX: Library class implementation

--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
+import pino from "pino";
+
 const pinoLogger = require('pino');
 
-function buildLogger(nameModule: string) {
-	const levels = {
-		emerg: 80, alert: 70, crit: 60, error: 50, warn: 40, info: 30, notice: 20, debug: 10,
-	};
+export class buildLogger extends pinoLogger {
+    constructor(nameModule: string) {
+        const levels = {
+            emerg: 80, alert: 70, crit: 60, error: 50, warn: 40, info: 30, notice: 20, debug: 10,
+        };
+        let module = nameModule;
 
-	let streams = [{stream: pinoLogger.destination({dest: '../' + nameModule + '.log', sync: false})},];
+        let streams = [{stream: buildLogger.destination({dest: '../' + module + '.log', sync: false})},];
 
-	if (process.env.NODE_ENV !== 'production') {
-		streams = [{stream: process.stdout}, {
-			stream: pinoLogger.destination({
-				dest: '../' + nameModule + '.log', sync: false
-			})
-		},];
-	}
+        if (process.env.NODE_ENV !== 'production') {
+            streams = [{stream: process.stdout}, {
+                stream: buildLogger.destination({
+                    dest: '../' + module + '.log', sync: false
+                })
+            },];
+        }
 
-	return pinoLogger({
-		level: 10, customLevels: levels, useOnlyCustomLevels: true, timestamp: () => {
-			return `,"time":"${new Date().toLocaleString("es-MX", {timeZone: 'America/Mexico_City'})}"`;
-		}, formatters: {
-			level: (label:string) => {
-				return {level: label};
-			},
-		},
-	}, pinoLogger.multistream(streams));
-
+        super({
+            level: 10, customLevels: levels, useOnlyCustomLevels: true, timestamp: () => {
+                return `,"time":"${new Date().toLocaleString("es-MX", {timeZone: 'America/Mexico_City'})}"`;
+            }, formatters: {
+                level: (label: string) => {
+                    return {level: label};
+                },
+            },
+        }, pinoLogger.multistream(streams));
+    }
 }
-
-export {
-	buildLogger
-};
